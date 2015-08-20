df8f28abc0076fa3ef8168b3fb525ae78555a0c5
Hom-Wang <wenhom.wang@gmail.com>
2015-08-20 23:55:50 +0800

    Add BSM_PeripheralHAL_UART 08/20

---
+++ Software/BSM_PeripheralHAL_UART/Program/experiment_stm32f0_msp.c
@@ -0,0 +1,44 @@
+/*====================================================================================================*/
+/*====================================================================================================*/
+#include "Dirvers\stm32f0_system.h"
+
+#include "experiment_stm32f0.h"
+/*====================================================================================================*/
+/*====================================================================================================*/
+/*
+System Clock source            = PLL (HSE)
+SYSCLK(Hz)                     = 48000000
+HCLK(Hz)                       = 48000000
+AHB Prescaler                  = 1
+APB1 Prescaler                 = 1
+HSE Frequency(Hz)              = 8000000
+PREDIV                         = 2
+PLLMUL                         = 12
+Flash Latency(WS)              = 1
+*/
+void HAL_MspInit( void )
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef state = HAL_OK;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PREDIV     = RCC_PREDIV_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL12;
+  state = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(state != HAL_OK)
+    while(1) { ; }
+
+  RCC_ClkInitStruct.ClockType      = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  state = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+  if(state != HAL_OK)
+    while(1) { ; }
+}
+/*====================================================================================================*/
+/*====================================================================================================*/
