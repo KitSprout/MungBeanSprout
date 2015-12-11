4dad0530f69e3a932770e1049de065e19ca0575d
Hom-Wang <wenhom.wang@gmail.com>
2015-12-12 02:14:16 +0800

    Update BSM_PeripheralHAL_TIM-PWM 12/12

--- Software/BSM_PeripheralHAL_TIM-PWM/Program/experiment_stm32f0.c
+++ Software/BSM_PeripheralHAL_TIM-PWM/Program/experiment_stm32f0.c
@@ -1,38 +1,42 @@
 /*=====================================================================================================*/
 /*=====================================================================================================*/
-#include "Dirvers\stm32f0_system.h"
+#include "drivers\stm32f0_system.h"
 
 #include "experiment_stm32f0.h"
 /*=====================================================================================================*/
 /*=====================================================================================================*/
-#define PWM_MOTOR_MIN 0
-#define PWM_MOTOR_MED 500
-#define PWM_MOTOR_MAX 1000
+#define PWM_MIN 0
+#define PWM_MED 50
+#define PWM_MAX 100
 
 #define PWM TIM3->CCR1  // PA6
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+void System_Init( void )
+{
+  HAL_Init();
+  TIM_PWM_Config();
+}
 
-static void TIM_PWM_Config( void );
-/*=====================================================================================================*/
-/*=====================================================================================================*/
 int main( void )
 {
-  uint16_t i = 0, state = 0;
+  uint8_t state = 0;
+  uint32_t i = PWM_MIN;
 
-  HAL_Init();
+  System_Init();
 
-  TIM_PWM_Config();
-
-  while (1) {
-    i = (state) ? (i-1) : (i+1);
+  while(1) {
+    
+    i = (state) ? (i - 1) : (i + 1);
     PWM = i;
-    if(i == PWM_MOTOR_MAX)  state = 1;
-    if(i == PWM_MOTOR_MIN) {state = 0;  Delay_100ms(10);}
-    Delay_1ms(1);
+    if(i == PWM_MAX){ state = 1; }
+    if(i == PWM_MIN){ state = 0; Delay_1ms(1000);}
+    Delay_10ms(4);
   }
 }
 /*====================================================================================================*/
 /*====================================================================================================*/
-static void TIM_PWM_Config( void )
+void TIM_PWM_Config( void )
 {
   GPIO_InitTypeDef GPIO_InitStruct;
   TIM_HandleTypeDef TIM_HandleStruct;
@@ -52,8 +56,8 @@ static void TIM_PWM_Config( void )
 
   /* TIM1 use APB1 = 48 MHz */
   TIM_HandleStruct.Instance               = TIM3;
-  TIM_HandleStruct.Init.Prescaler         = 48 - 1;
-  TIM_HandleStruct.Init.Period            = 1000 - 1;
+  TIM_HandleStruct.Init.Prescaler         = 480 - 1;
+  TIM_HandleStruct.Init.Period            = PWM_MAX - 1;
   TIM_HandleStruct.Init.ClockDivision     = 0;
   TIM_HandleStruct.Init.CounterMode       = TIM_COUNTERMODE_UP;
   TIM_HandleStruct.Init.RepetitionCounter = 0;
@@ -62,11 +66,11 @@ static void TIM_PWM_Config( void )
   TIM_OC_InitStruct.OCMode       = TIM_OCMODE_PWM2;
   TIM_OC_InitStruct.OCFastMode   = TIM_OCFAST_DISABLE;
   TIM_OC_InitStruct.OCPolarity   = TIM_OCPOLARITY_LOW;
-  TIM_OC_InitStruct.OCNPolarity  = TIM_OCPOLARITY_LOW;
+  TIM_OC_InitStruct.OCNPolarity  = TIM_OCNPOLARITY_HIGH;
   TIM_OC_InitStruct.OCIdleState  = TIM_OCIDLESTATE_SET;
   TIM_OC_InitStruct.OCNIdleState = TIM_OCNIDLESTATE_RESET;
 
-  TIM_OC_InitStruct.Pulse = 800;
+  TIM_OC_InitStruct.Pulse = PWM_MED;
   HAL_TIM_PWM_ConfigChannel(&TIM_HandleStruct, &TIM_OC_InitStruct, TIM_CHANNEL_1);
 
   HAL_TIM_PWM_Start(&TIM_HandleStruct, TIM_CHANNEL_1);
