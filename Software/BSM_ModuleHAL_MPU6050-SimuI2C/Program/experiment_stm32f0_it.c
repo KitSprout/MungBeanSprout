4cd22d75a1c701a603012bc7c5c041b66f35394f
Hom-Wang <wenhom.wang@gmail.com>
2015-12-12 02:23:56 +0800

    Add BSM_ModuleHAL_MPU6050-SimuI2C 12/12

---
+++ Software/BSM_ModuleHAL_MPU6050-SimuI2C/Program/experiment_stm32f0_it.c
@@ -0,0 +1,34 @@
+/*====================================================================================================*/
+/*====================================================================================================*/
+#include "drivers\stm32f0_system.h"
+/*====================================================================================================*/
+/*====================================================================================================*/
+void NMI_Handler( void ) {}
+void HardFault_Handler( void ) { while(1); }
+void SVC_Handler( void ) {}
+void PendSV_Handler( void ) {}
+void SysTick_Handler( void ) { HAL_IncTick(); }
+/*====================================================================================================*/
+/*====================================================================================================*/
+//void WWDG_IRQHandler(void);
+//void RTC_IRQHandler(void);
+//void FLASH_IRQHandler(void);
+//void RCC_IRQHandler(void);
+//void EXTI0_1_IRQHandler(void);
+//void EXTI2_3_IRQHandler(void);
+//void EXTI4_15_IRQHandler(void);
+//void DMA1_Channel1_IRQHandler(void);
+//void DMA1_Channel2_3_IRQHandler(void);
+//void DMA1_Channel4_5_IRQHandler(void);
+//void ADC1_IRQHandler(void);
+//void TIM1_BRK_UP_TRG_COM_IRQHandler(void);
+//void TIM1_CC_IRQHandler(void);
+//void TIM3_IRQHandler(void);
+//void TIM14_IRQHandler(void);
+//void TIM16_IRQHandler(void);
+//void TIM17_IRQHandler(void);
+//void I2C1_IRQHandler(void);
+//void SPI1_IRQHandler(void);
+//void USART1_IRQHandler(void);
+/*====================================================================================================*/
+/*====================================================================================================*/
