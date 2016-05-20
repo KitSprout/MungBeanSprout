8d52cfda84b8656734a1630d8e3732ea960d2a74
Hom-Wang <wenhom.wang@gmail.com>
2016-05-21 03:35:28 +0800

    Add BSM_PeripheralSTD_GPIO 05/21

---
+++ Software/BSM_PeripheralSTD_GPIO/Program/beanSproutM_bsp.c
@@ -0,0 +1,48 @@
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+#include "drivers\stm32f0_system.h"
+
+#include "beanSproutM_bsp.h"
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+void BSM_GPIO_Config( void )
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* Enable all GPIO Clk *******************************************************/
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+
+  /* GPIO all analog input *****************************************************/
+  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_All;
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_AN;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_All & (~(GPIO_Pin_13 | GPIO_Pin_14));
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* GPIO Pin ******************************************************************/
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0  | GPIO_Pin_1  | GPIO_Pin_2  | GPIO_Pin_3  | GPIO_Pin_4  |
+                             GPIO_Pin_5  | GPIO_Pin_6  | GPIO_Pin_7  | GPIO_Pin_9  | GPIO_Pin_10;
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_1;
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* GPIO Reset Pin ************************************************************/
+  GPIO_ResetBits(GPIOA, GPIO_Pin_0  | GPIO_Pin_1  | GPIO_Pin_2  | GPIO_Pin_3  | GPIO_Pin_4  |
+                        GPIO_Pin_5  | GPIO_Pin_6  | GPIO_Pin_7  | GPIO_Pin_9  | GPIO_Pin_10);
+  GPIO_ResetBits(GPIOB, GPIO_Pin_1);
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*/
