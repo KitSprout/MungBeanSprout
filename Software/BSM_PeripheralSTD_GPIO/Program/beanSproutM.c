677b3f733e32c272664146ebfe284b21b72a7751
Hom-Wang <wenhom.wang@gmail.com>
2016-08-18 15:47:18 +0800

    Update BSM_PeripheralSTD_GPIO 08/18

--- Software/BSM_PeripheralSTD_GPIO/Program/beanSproutM.c
+++ Software/BSM_PeripheralSTD_GPIO/Program/beanSproutM.c
@@ -7,23 +7,28 @@
 /*=====================================================================================================*/
 void BSM_Init( void )
 {
-  SystemInit();
-  HAL_InitTick();
-
   BSM_GPIO_Config();
 }
 /*=====================================================================================================*/
 /*=====================================================================================================*/
+#define GPIO_A_PINs       GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_9 | GPIO_Pin_10
+#define GPIO_A_GPIO_PORT  GPIOA
+#define GPIO_A_Set()      GPIO_SetBits(GPIO_A_GPIO_PORT, GPIO_A_PINs)
+#define GPIO_A_Reset()    GPIO_ResetBits(GPIO_A_GPIO_PORT, GPIO_A_PINs)
+
+#define GPIO_B_PINs       GPIO_Pin_1
+#define GPIO_B_GPIO_PORT  GPIOB
+#define GPIO_B_Set()      GPIO_SetBits(GPIO_B_GPIO_PORT, GPIO_B_PINs)
+#define GPIO_B_Reset()    GPIO_ResetBits(GPIO_B_GPIO_PORT, GPIO_B_PINs)
+
 void BSM_Loop( void )
 {
   while(1) {
-    GPIO_SetBits(GPIOA, GPIO_Pin_0  | GPIO_Pin_1  | GPIO_Pin_2  | GPIO_Pin_3  | GPIO_Pin_4  |
-                        GPIO_Pin_5  | GPIO_Pin_6  | GPIO_Pin_7  | GPIO_Pin_9  | GPIO_Pin_10);
-    GPIO_SetBits(GPIOB, GPIO_Pin_1);
+    GPIO_A_Set();
+    GPIO_B_Set();
     delay_ms(100);
-    GPIO_ResetBits(GPIOA, GPIO_Pin_0  | GPIO_Pin_1  | GPIO_Pin_2  | GPIO_Pin_3  | GPIO_Pin_4  |
-                          GPIO_Pin_5  | GPIO_Pin_6  | GPIO_Pin_7  | GPIO_Pin_9  | GPIO_Pin_10);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_1);
+    GPIO_A_Reset();
+    GPIO_B_Reset();
     delay_ms(100);
   }
 }
--- Software/BSM_PeripheralSTD_GPIO/Program/beanSproutM_bsp.c
+++ Software/BSM_PeripheralSTD_GPIO/Program/beanSproutM_bsp.c
@@ -5,6 +5,16 @@
 #include "beanSproutM_bsp.h"
 /*=====================================================================================================*/
 /*=====================================================================================================*/
+#define GPIO_A_PINs       GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_9 | GPIO_Pin_10
+#define GPIO_A_GPIO_PORT  GPIOA
+#define GPIO_A_Set()      GPIO_SetBits(GPIO_A_GPIO_PORT, GPIO_A_PINs)
+#define GPIO_A_Reset()    GPIO_ResetBits(GPIO_A_GPIO_PORT, GPIO_A_PINs)
+
+#define GPIO_B_PINs       GPIO_Pin_1
+#define GPIO_B_GPIO_PORT  GPIOB
+#define GPIO_B_Set()      GPIO_SetBits(GPIO_B_GPIO_PORT, GPIO_B_PINs)
+#define GPIO_B_Reset()    GPIO_ResetBits(GPIO_B_GPIO_PORT, GPIO_B_PINs)
+
 void BSM_GPIO_Config( void )
 {
   GPIO_InitTypeDef GPIO_InitStruct;
@@ -24,25 +34,20 @@ void BSM_GPIO_Config( void )
   GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* GPIO Pin ******************************************************************/
-  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0  | GPIO_Pin_1  | GPIO_Pin_2  | GPIO_Pin_3  | GPIO_Pin_4  |
-                             GPIO_Pin_5  | GPIO_Pin_6  | GPIO_Pin_7  | GPIO_Pin_9  | GPIO_Pin_10;
   GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_1;
-  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin   = GPIO_A_PINs;
+  GPIO_Init(GPIO_A_GPIO_PORT, &GPIO_InitStruct);
+
+  GPIO_InitStruct.GPIO_Pin   = GPIO_B_PINs;
+  GPIO_Init(GPIO_B_GPIO_PORT, &GPIO_InitStruct);
 
   /* GPIO Reset Pin ************************************************************/
-  GPIO_ResetBits(GPIOA, GPIO_Pin_0  | GPIO_Pin_1  | GPIO_Pin_2  | GPIO_Pin_3  | GPIO_Pin_4  |
-                        GPIO_Pin_5  | GPIO_Pin_6  | GPIO_Pin_7  | GPIO_Pin_9  | GPIO_Pin_10);
-  GPIO_ResetBits(GPIOB, GPIO_Pin_1);
+  GPIO_A_Reset();
+  GPIO_B_Reset();
 }
 /*=====================================================================================================*/
 /*=====================================================================================================*/
